653de6b5bf07d1a69794c4089494e55ef832f892
IAmHeroForFun <everythinga1360@gmail.com>
2026-09-20 16:59:15 +0530

    fix: explicitly register food_images library in TEMPLATES and guard detail URLs

--- dietary_app/settings.py
+++ dietary_app/settings.py
@@ -51,6 +51,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'food_images': 'core.templatetags.food_images',
+            },
         },
     },
 ]
